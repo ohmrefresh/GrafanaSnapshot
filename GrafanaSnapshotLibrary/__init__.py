b60b025157f8748f2ccf3c5bbfe86ee1728396f5
Panchorn Lertvipada <nonpcn@gmail.com>
2019-07-22 09:54:47 +0700

    Remove annotation when create snapshot

--- GrafanaSnapshotLibrary/__init__.py
+++ GrafanaSnapshotLibrary/__init__.py
@@ -4,24 +4,20 @@ import argparse
 import os
 import datetime
 import requests
-from dotenv import load_dotenv
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from PyInquirer import (Token, ValidationError, Validator, prompt, style_from_dict)
+import time
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 name = "GrafanaSnapshotLibrary"
 
-load_dotenv()
-API_TOKEN = os.getenv("API_TOKEN")
-BASE_URL = os.getenv("BASE_URL")
-
-
-def get_annotations(time_from=None, time_to=None, alert_id=None, dashboard_id=None, panel_id=None, tags=[], limit=None):
-    api_ep = "{}/api/annotations".format(BASE_URL)
+def get_annotations(url, token, time_from=None, time_to=None, alert_id=None, dashboard_id=None, panel_id=None, tags=[], limit=None):
+    api_ep = "{}/api/annotations".format(url)
     method = "GET"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer {}".format(API_TOKEN)
+        "Authorization": "Bearer {}".format(token)
     }
 
     params = {}
@@ -49,12 +45,11 @@ def get_annotations(time_from=None, time_to=None, alert_id=None, dashboard_id=No
     return response.json()
 
 
-def create_annotations(tags=None, time_from=None,
-                       time_to=None):
-    api_ep = "{}/api/annotations".format(BASE_URL)
+def create_annotations(url, token, tags=[], time_from=None, time_to=None):
+    api_ep = "{}/api/annotations".format(url)
     method = "POST"
     headers = {
-        "Authorization": "Bearer {}".format(API_TOKEN),
+        "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
     }
 
@@ -62,7 +57,7 @@ def create_annotations(tags=None, time_from=None,
         "time": time_from,
         "timeEnd": time_to,
         "isRegion": bool('false'),
-        "tags": [tags],
+        "tags": tags,
         "text": "Test"
     }
 
@@ -76,7 +71,7 @@ def create_annotations(tags=None, time_from=None,
     return response.json()
 
 
-def delete_annotations_by_region_id(region_id=None):
+def delete_annotations_by_region_id(url, token, region_id=None):
 
     """
     Delete Annotation By RegionId  (https://grafana.com/docs/http_api/annotations/#delete-annotation-by-regionid)
@@ -88,10 +83,10 @@ def delete_annotations_by_region_id(region_id=None):
     :return a json:
     """
 
-    api_ep = "{}/api/annotations/region/{}".format(BASE_URL, region_id)
+    api_ep = "{}/api/annotations/region/{}".format(url, region_id)
     method = "DELETE"
     headers = {
-        "Authorization": "Bearer {}".format(API_TOKEN),
+        "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
     }
 
@@ -104,11 +99,11 @@ def delete_annotations_by_region_id(region_id=None):
     return response.json()
 
 
-def get_dashboard(slug):
-    api_ep = "{}/api/dashboards/db/{}".format(BASE_URL, slug)
+def get_dashboard(url, token, slug):
+    api_ep = "{}/api/dashboards/db/{}".format(url, slug)
     method = "GET"
     headers = {
-        "Authorization": "Bearer {}".format(API_TOKEN),
+        "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
     }
 
@@ -121,19 +116,19 @@ def get_dashboard(slug):
     return response.json()
 
 
-def search_dashboards(query=None, tag=None, starred=None, tagcloud=None):
-    api_ep = "{}/api/search".format(BASE_URL)
+def search_dashboards(url, token, query=None, tags=[], starred=None, tagcloud=None):
+    api_ep = "{}/api/search".format(url)
     method = "GET"
     headers = {
-        "Authorization": "Bearer {}".format(API_TOKEN),
+        "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
     }
 
     params = {}
     if query:
         params["query"] = query
-    if tag:
-        params["tag"] = tag
+    if tags:
+        params["tag"] = tags
     if starred:
         params["starred"] = starred
     if tagcloud:
@@ -149,12 +144,12 @@ def search_dashboards(query=None, tag=None, starred=None, tagcloud=None):
     return response.json()
 
 
-def create_snapshot(dashboard, name=None, expire=None, external=None, key=None, deleteKey=None, time_from=None,
+def create_snapshot(url, token, dashboard, name=None, expire=None, external=None, key=None, deleteKey=None, time_from=None,
                     time_to=None):
-    api_ep = "{}/api/snapshots".format(BASE_URL)
+    api_ep = "{}/api/snapshots".format(url)
     method = "POST"
     headers = {
-        "Authorization": "Bearer {}".format(API_TOKEN),
+        "Authorization": "Bearer {}".format(token),
         "Content-Type": "application/json"
     }
 
@@ -197,53 +192,79 @@ def datetime_string_to_timestamp(date_time_str):
     return int(datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S').strftime("%s"))
 
 
-def get_dashboard_url(key):
-    return "{}/dashboard/snapshot/{}".format(BASE_URL,key)
+def get_dashboard_u(url, key):
+    return "{}/dashboard/snapshot/{}".format(url, key)
 
 
-def main():
+def run():
+    info = askInformation()
+    url = info.get('base_url')
+    token = info.get('api_token')
+
     parser = argparse.ArgumentParser(description='Create Snapshot on Grafana with official api (https://grafana.com/docs/http_api/)')
 
-    parser.add_argument('-s',"--start",metavar='start', help='Start date time ex. 1563183710618',type=int,  required=True)
-    parser.add_argument('-e',"--end",metavar='end', help='End date time ex. 1563185212275', type=int, required=True)
-    parser.add_argument('-t',"--tags",metavar='tags', help='Tag in grafana ex. tag-name,tag-name1',
-                        required=True)
+    parser.add_argument('-s', "--start", metavar='start', help='Start date time ex. 1563183710618', type=int, required=True)
+    parser.add_argument('-e', "--end", metavar='end', help='End date time ex. 1563185212275', type=int, required=True)
+    parser.add_argument('-t', "--tags", metavar='tags', help='Tag in grafana ex. tag-name,tag-name1', required=True)
     args = parser.parse_args()
 
     date_time_start = int(args.start)
     date_time_end = int(args.end)
     tags = args.tags
 
-    # create annotations with tags, start , end
-    annotation_region_id = create_annotations(tags, date_time_start, date_time_end)['id']
-    dashboards_info = search_dashboards(tag=tags)
-    dashboards = {}
+    dashboards_info = search_dashboards(url, token, tags=[tags])
+    snapshot_list = {}
     for dashboard_info in dashboards_info:
         slug = os.path.basename(dashboard_info["uri"])
-        print(slug)
-        dashboards[slug] = get_dashboard(slug)
-
-    annotations = get_annotations(tags=[tags], time_from=date_time_start, time_to=date_time_end)
-
-    time_regions = {}
-    for regionId in set(map(lambda x: x["regionId"], annotations)):
-        time_pair = sorted([a["time"] for a in annotations if a["regionId"] == regionId])
-        region_str = "{0[0]}_{0[1]}".format(tuple(map(time_str_from_unix_ms, time_pair)))
-        time_regions[region_str] = time_pair
-
-    snapshot_list = []
-    for slug, dashboard in dashboards.items():
-        for region_str, v in time_regions.items():
-            snapshot_name = "{}_{}".format(slug, region_str)
-            time_from = v[0]
-            time_to = v[1]
-            snapshot_list.append(get_dashboard_url(create_snapshot(dashboard, name=snapshot_name, time_from=time_from, time_to=time_to)['key']))
-
-    # Remove annotations
-    delete_annotations_by_region_id(annotation_region_id)
-
+        region_str = "{0[0]}_{0[1]}".format(tuple(map(time_str_from_unix_ms, [date_time_start, date_time_end])))
+        snapshot_name = "{}_{}".format(slug, region_str)
+        snapshot_list[snapshot_name] = get_dashboard_u(url, create_snapshot(url,
+                                                                            token,
+                                                                            get_dashboard(url, token, slug),
+                                                                            name=snapshot_name,
+                                                                            time_from=date_time_start,
+                                                                            time_to=date_time_end)['key'])
     print(snapshot_list)
 
+class EmptyValidator(Validator):
+    def validate(self, value):
+        if len(value.text):
+            return True
+        else:
+            raise ValidationError(
+                message="You can't leave this blank",
+                cursor_position=len(value.text))
+
+style = style_from_dict({
+    Token.QuestionMark: '#fac731 bold',
+    Token.Answer: '#4688f1 bold',
+    Token.Instruction: '',  # default
+    Token.Separator: '#cc5454',
+    Token.Selected: '#0abf5b',  # default
+    Token.Pointer: '#673ab7 bold',
+    Token.Question: '',
+})
+
+
+def askInformation():
+    questions = [
+        {
+            'type': 'input',
+            'name': 'base_url',
+            'message': 'Grafana url >',
+            'default': 'https://127.0.0.1:3000',
+            'validate': EmptyValidator
+        },
+        {
+            'type': 'password',
+            'name': 'api_token',
+            'message': 'API token >',
+            'default': 'xxx=',
+            'validate': EmptyValidator
+        }
+    ]
+    answers = prompt(questions, style=style)
+    return answers
 
 if __name__ == "__main__":
-    main()
+    run()
